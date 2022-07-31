f5e9fcf67d40c024d095761a9cacfdec24a6d7ae
Sanjay Sunar <sansunar11@gmail.com>
2022-07-31 15:42:58 +0530

    UpdateStoryActivity
    - get data from an intent

--- app/src/main/java/com/sanjay/interactivestory/StoryActivity.java
+++ app/src/main/java/com/sanjay/interactivestory/StoryActivity.java
@@ -1,7 +1,9 @@
 package com.sanjay.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StoryActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class StoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        intent.getStringExtra("name");
+        
     }
 }
